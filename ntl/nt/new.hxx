ea1208a97133e88e9b172ec9bda33d813602f306
icestudent <icestudent@gmail.com>
2008-03-21 23:52:18 +0000

    removed inline specifiers :(

--- ntl/nt/new.hxx
+++ ntl/nt/new.hxx
@@ -14,28 +14,24 @@
 #include <new>
 
 ///\name  Single-object forms
-__forceinline
 void * __cdecl
   operator new(std::size_t size) throw(std::bad_alloc)
 {
   return ntl::nt::heap::alloc(ntl::nt::process_heap(), size);
 }
 
-__forceinline
 void __cdecl
   operator delete(void* ptr) throw()
 {
   ntl::nt::heap::free(ntl::nt::process_heap(), ptr);
 }
 
-__forceinline
 void * __cdecl
   operator new(std::size_t size, const std::nothrow_t&) throw()
 {
   return ntl::nt::heap::alloc(ntl::nt::process_heap(), size);
 }
 
-__forceinline
 void __cdecl
   operator delete(void* ptr, const std::nothrow_t&) throw()
 {
@@ -45,28 +41,24 @@ void __cdecl
 
 ///\name  Array forms
 
-__forceinline
 void * __cdecl
   operator new[](std::size_t size) throw(std::bad_alloc)
 {
   return ntl::nt::heap::alloc(ntl::nt::process_heap(), size);
 }
 
-__forceinline
 void __cdecl
   operator delete[](void* ptr) throw()
 {
   ntl::nt::heap::free(ntl::nt::process_heap(), ptr);
 }
 
-__forceinline
 void * __cdecl
   operator new[](std::size_t size, const std::nothrow_t&) throw()
 {
   return ntl::nt::heap::alloc(ntl::nt::process_heap(), size);
 }
 
-__forceinline
 void __cdecl
   operator delete[](void* ptr, const std::nothrow_t&) throw()
 {
