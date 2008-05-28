1f28cc8c0c99bfcb6be825f84ded7f4678daadcb
icestudent <icestudent@gmail.com>
2008-05-28 20:05:33 +0000

    confirming 'utility' to the N2606

--- ntl/stlx/utility.hxx
+++ ntl/stlx/utility.hxx
@@ -24,7 +24,8 @@ namespace std {
  *@{
  */
 
-/// Operators [20.2.1 lib.operators]
+#pragma region operators
+/// Operators [20.2.1 operators]
 namespace rel_ops {
 
 template<class T> inline
@@ -40,9 +41,19 @@ template<class T> inline
 bool operator>=(const T & x, const T & y) { return !(x < y); }
 
 }//namespace rel_ops
+#pragma endregion
 
+#pragma region forward
+/// 20.2.2 forward/move helpers [forward]
+#ifdef NTL__CXX
+template <class T> struct identity;
+template <class T> T&& forward(typename identity<T>::type&&);
+template <class T> typename remove_reference<T>::type&& move(T&&);
+#endif
+#pragma endregion
 
-/// Pairs [20.2.2 lib.pairs]
+#pragma region pairs
+/// Pairs [20.2.3 pairs]
 template<class T1, class T2>
 struct pair
 {
@@ -57,7 +68,45 @@ struct pair
 
     template<class U, class V>
     pair(const pair<U, V> & p) : first(p.first), second(p.second) {}
+
+#ifdef NTL__CXX
+    pair(pair&& p);
+    pair(const T1& x, const T2& y);
+    template<class U, class V> pair(U&& x, V&& y);
+    template<class U, class V> pair(pair<U, V>&& p);
+    template<class U, class... Args> pair(U&& x, Args&&... args);
+
+    // allocator-extended constructors
+    template <class Alloc> pair(allocator_arg_t, const Alloc& a);
+    template <class Alloc> pair(allocator_arg_t, const Alloc& a, const T1& x, const T2& y);
+    
+    template <class U, class V, class Alloc>
+    pair(allocator_arg_t, const Alloc& a, U&& x, V&& y);
+
+    template <class Alloc> pair(allocator_arg_t, const Alloc&, pair&& p);
+
+    template <class U, class V, class Alloc>
+    pair(allocator_arg_t, const Alloc& a, const pair<U, V>& p);
+
+    template <class U, class V, class Alloc>
+    pair(allocator_arg_t, const Alloc& a, pair<U, V>&& p);
+
+    template <class U, class... Args, class Alloc>
+    pair(allocator_arg_t, const Alloc& a, U&& x, Args&&... args);
+
+    pair& operator=(pair&& p);
+
+    template<class U, class V> pair& operator=(pair<U, V>&& p);
+
+    void swap(pair&& p);
+#endif
 };
+
+template <class T1, class T2, class Alloc>
+struct uses_allocator<pair<T1, T2>, Alloc>;
+
+template <class T1, class T2>
+struct constructible_with_allocator_prefix<pair<T1, T2>{>};
 
 ///\name  Comparisons
 ///@{
@@ -120,6 +169,7 @@ pair<T1, T2>
 { 
   return pair<T1, T2>( x, y );
 }
+#pragma endregion
 
 /**@} lib_utility */
 /**@} lib_utilities */
