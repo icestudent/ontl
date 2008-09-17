b2b7b69aaafa0ad77cfc66c7d3957a0f021524d3
icestudent <icestudent@gmail.com>
2008-09-17 13:03:07 +0000

    fix

--- ntl/stlx/map.hxx
+++ ntl/stlx/map.hxx
@@ -146,8 +146,7 @@ public:
     {
       if(this != &x){
         val_comp_ = x.val_comp_;
-        clear();
-        insert_range(x.cbegin(), x.cend());
+        tree_type::operator=(x);
       }
       return *this;
     }
