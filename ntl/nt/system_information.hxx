87daa5fc4fa6241519e9170ccadbc4a83865c368
icestudent <icestudent@gmail.com>
2008-09-29 06:00:21 +0000

    f

--- ntl/nt/system_information.hxx
+++ ntl/nt/system_information.hxx
@@ -166,7 +166,7 @@ class system_information_base
         delete[] ptr;
         if ( s == status::info_length_mismatch )
           continue;
-        ptr = std::nullptr;
+        ptr = nullptr;
         break;
       }
     }
@@ -189,7 +189,7 @@ class system_information_base
       query(
         void *          system_nformation,
         unsigned long   system_information_length,
-        unsigned long * return_length = std::nullptr
+        unsigned long * return_length = nullptr
         )
     {
       return QueryInformation(info_class::info_class_type,
