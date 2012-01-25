d6a99056597599e1ea64e919369e975370c3c410
icestudent <icestudent@gmail.com>
2012-01-25 09:01:14 +0000

    csd fix

--- ntl/stlx/cstd/stdio.h
+++ ntl/stlx/cstd/stdio.h
@@ -1,2 +1,4 @@
 #include "../cstdio.hxx"
 
+int NTL__CRTCALL printf(const char* format, ...);
+NTL__EXTERNAPI int __cdecl sscanf(const char* string, const char* format, ...);
--- ntl/stlx/cstd/time.h
+++ ntl/stlx/cstd/time.h
@@ -2,6 +2,8 @@
 
 using std::time_t;
 using std::clock_t;
+using std::tm;
 
 using std::time;
+using std::localtime;
 using std::clock;
