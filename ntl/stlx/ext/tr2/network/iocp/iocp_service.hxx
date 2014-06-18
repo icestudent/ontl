d7ad8e45579486b795014e5c605f2ff5cc12dbea
icestudent <icestudent@gmail.com>
2014-06-18 19:22:10 +0400

    whitespace change

--- ntl/stlx/ext/tr2/network/iocp/iocp_service.hxx
+++ ntl/stlx/ext/tr2/network/iocp/iocp_service.hxx
@@ -11,58 +11,58 @@
 
 namespace std { namespace tr2 { namespace sys {
 
-	class iocp_service:
+  class iocp_service:
     public io_service::service
-	{
-		typedef ntl::nt::ntstatus ntstatus;
-		typedef ntl::nt::status   status;
-		typedef __::async_operation async_operation;
+  {
+    typedef ntl::nt::ntstatus ntstatus;
+    typedef ntl::nt::status   status;
+    typedef __::async_operation async_operation;
 
-		// system codes
-		static const ntl::nt::ntstatus stop_service_code = ntl::nt::status::system_shutdown;
+    // system codes
+    static const ntl::nt::ntstatus stop_service_code = ntl::nt::status::system_shutdown;
 
     __::timer_scheduler& scheduler;
-	public:
-		static io_service::id id;
+  public:
+    static io_service::id id;
 
-		explicit iocp_service(io_service& ios)
-			: service(ios)
+    explicit iocp_service(io_service& ios)
+      : service(ios)
       , timer_event(timer_event.auto_reset)
-			, timer_thread(&iocp_service::timer_proc, this)
+      , timer_thread(&iocp_service::timer_proc, this)
       , scheduler(use_service<__::timer_scheduler>(ios))
-		{
+    {
       scheduler.handler =
         &iocp_service::add_timer_;
       scheduler.ctx = this;
         //std::bind(&iocp_service::add_timer, this, std::placeholders::_1, std::placeholders::_2);
-		}
+    }
 
-		bool stopped() const
-		{
-			return this->stopper.test();
-		}
+    bool stopped() const
+    {
+      return this->stopper.test();
+    }
 
-		void stop()
-		{
-			if(stopper.test_and_set() == false) {
-				check(iocp.set_completion(nullptr, stop_service_code));
-			}
-		}
+    void stop()
+    {
+      if(stopper.test_and_set() == false) {
+        check(iocp.set_completion(nullptr, stop_service_code));
+      }
+    }
 
-		void reset()
-		{
-			stopper.clear();
-		}
+    void reset()
+    {
+      stopper.clear();
+    }
 
-		size_t run_one(error_code& ec)
-		{
-			return do_poll(true, ec);
-		}
+    size_t run_one(error_code& ec)
+    {
+      return do_poll(true, ec);
+    }
 
-		size_t poll_one(error_code& ec)
-		{
-			return do_poll(false, ec);
-		}
+    size_t poll_one(error_code& ec)
+    {
+      return do_poll(false, ec);
+    }
 
     template<class CompletionHandler>
     void dispatch(CompletionHandler handler);
@@ -70,39 +70,39 @@ namespace std { namespace tr2 { namespace sys {
     template<class CompletionHandler>
     void post(CompletionHandler handler);
 
-	protected:
-		void check(ntl::nt::ntstatus st)
-		{
-			if(!ntl::nt::success(st)) {
-				std::error_code ec = std::make_error_code(st);
-				throw_system_error(ec);
-			}
-		}
+  protected:
+    void check(ntl::nt::ntstatus st)
+    {
+      if(!ntl::nt::success(st)) {
+        std::error_code ec = std::make_error_code(st);
+        throw_system_error(ec);
+      }
+    }
 
-		void work_started()
-		{
-			++workers;
-		}
+    void work_started()
+    {
+      ++workers;
+    }
 
-		void work_finished()
-		{
-			if(--workers == 0)
-				stop();
-		}
+    void work_finished()
+    {
+      if(--workers == 0)
+        stop();
+    }
 
-		void add_timer(const void* timer, async_operation* op)
-		{
-			if(shutdown.test()) {
-				post_immediate_completion(op);
-				return;
-			}
+    void add_timer(const void* timer, async_operation* op)
+    {
+      if(shutdown.test()) {
+        post_immediate_completion(op);
+        return;
+      }
 
-			work_started();
+      work_started();
 
-			wlock lock(timer_lock);
+      wlock lock(timer_lock);
       timers.push_back(std::make_pair(reinterpret_cast<ntl::nt::legacy_handle>(timer), op));
       timer_event.pulse();
-		}
+    }
 
     /** Request invocation of the given operation and return immediately. */
     void post_immediate_completion(async_operation* op)
@@ -124,72 +124,72 @@ namespace std { namespace tr2 { namespace sys {
       work_finished();
     }
 
-		bool do_poll(bool block, error_code& ec)
-		{
-			ec.clear();
-			if(workers.compare(0) == 0) {
-				stop();
-				return false;
-			}
+    bool do_poll(bool block, error_code& ec)
+    {
+      ec.clear();
+      if(workers.compare(0) == 0) {
+        stop();
+        return false;
+      }
 
-			const ntl::nt::system_duration instant;
-			for(;;) {
+      const ntl::nt::system_duration instant;
+      for(;;) {
 
-				ntl::nt::io_completion_port::entry entry;
-				ntstatus st = block ? iocp.pop_completion(entry) : iocp.pop_completion(entry, instant);
+        ntl::nt::io_completion_port::entry entry;
+        ntstatus st = block ? iocp.pop_completion(entry) : iocp.pop_completion(entry, instant);
 
-				if(st == ntl::nt::status::timeout) {
-					// no ready operations
-					if(block)
-						continue;
-					return false;
-				}
-				else if(!ntl::nt::success(st)) {
-					// iocp error
-					ec = std::make_error_code(st);
-					return false;
-				}
-				else if(entry.Status == stop_service_code) {
-					// stop service
-					if(stopper.test()) {
-						return false;
-					}
-				}else if(entry.Apc) {
-					const async_operation* op = static_cast<const async_operation*>(entry.Apc);
-					assert(op->is_async_operation());
+        if(st == ntl::nt::status::timeout) {
+          // no ready operations
+          if(block)
+            continue;
+          return false;
+        }
+        else if(!ntl::nt::success(st)) {
+          // iocp error
+          ec = std::make_error_code(st);
+          return false;
+        }
+        else if(entry.Status == stop_service_code) {
+          // stop service
+          if(stopper.test()) {
+            return false;
+          }
+        }else if(entry.Apc) {
+          const async_operation* op = static_cast<const async_operation*>(entry.Apc);
+          assert(op->is_async_operation());
 
-					if(op->ready.test()) {
+          if(op->ready.test()) {
             complete(op, std::make_error_code(entry.Status), entry.Information);
-						return true;
+            return true;
           } else {
             op = nullptr;
           }
-				}
-				continue;
-			}
-			return false;
-		}
+        }
+        continue;
+      }
+      return false;
+    }
 
-	private:
-		void shutdown_service() override
-		{
-			shutdown.set();
+  private:
+    void shutdown_service() override
+    {
+      shutdown.set();
       timer_event.set();
-			while(workers > 0) {
-				ntl::nt::io_completion_port::entry entry;
-				ntstatus st = iocp.pop_completion(entry);
-				if(entry.Apc) {
-					async_operation* op = static_cast<async_operation*>(const_cast<void*>(entry.Apc));
-					assert(op->is_async_operation());
-					op->destroy();
-					--workers;
-				}
-			}
+      while(workers > 0) {
+        ntl::nt::io_completion_port::entry entry;
+        ntstatus st = iocp.pop_completion(entry);
+        if(entry.Apc) {
+          async_operation* op = static_cast<async_operation*>(const_cast<void*>(entry.Apc));
+          assert(op->is_async_operation());
+          op->destroy();
+          --workers;
+        }
+      }
       const std::array<ntl::nt::legacy_handle,2> objects = {iocp.get(), timer_thread.get()};
       ntl::nt::wait_for(objects, true, true);
-			iocp.reset();
+      iocp.reset();
       timer_thread.reset();
-		}
+    }
 
     static uint32_t __stdcall timer_proc(void* Parameter)
     { return static_cast<iocp_service*>(Parameter)->timer_worker(); }
@@ -197,54 +197,54 @@ namespace std { namespace tr2 { namespace sys {
     { return static_cast<iocp_service*>(ctx)->add_timer(timer, op); }
 
     uint32_t __stdcall timer_worker()
-		{
-			using namespace ntl::nt;
-      this_thread::setname("iocp::timer_proc");
+    {
+      using namespace ntl::nt;
+      ntl::nt::this_thread::setname("iocp::timer_proc");
 
-			size_t count;
-			legacy_handle handles[64];
+      size_t count;
+      legacy_handle handles[64];
 
       handles[0] = timer_event.get();
 
-			do {
-				{
-					rlock lock(timer_lock);
-					size_t size = timers.size();
+      do {
+        {
+          rlock lock(timer_lock);
+          size_t size = timers.size();
           for(count = 0; count != size; ++count) {
-						handles[count+1] = timers[count].first;
+            handles[count+1] = timers[count].first;
           }
-				}
+        }
 
-				ntstatus st = NtWaitForMultipleObjects(count+1, handles, wait_type::WaitAny, false, infinite_timeout());
+        ntstatus st = NtWaitForMultipleObjects(count+1, handles, wait_type::WaitAny, false, infinite_timeout());
         if(st == ntl::nt::status::wait_0) {
           // timers changed
           continue;;
         }
-				else if(st >= status::wait_1 && st <= status::wait_63) {
-					count = st - status::wait_1;
-					wlock lock(timer_lock);
-					post_deferred_completion(timers[count].second);
+        else if(st >= status::wait_1 && st <= status::wait_63) {
+          count = st - status::wait_1;
+          wlock lock(timer_lock);
+          post_deferred_completion(timers[count].second);
           timers.erase(timers.begin() + count);
-				}
-			} while(shutdown.test(false));
+        }
+      } while(shutdown.test(false));
 
-			ntl::nt::this_thread::exit(status::success);
-		}
+      ntl::nt::this_thread::exit(status::success);
+    }
 
-	private:
-		ntl::nt::io_completion_port iocp;
-		ntl::atomic::value_t workers;
-		ntl::atomic::flag_t stopper, shutdown;
+  private:
+    ntl::nt::io_completion_port iocp;
+    ntl::atomic::value_t workers;
+    ntl::atomic::flag_t stopper, shutdown;
     //ntl::nt::user_event shutdown;
-		
-		// timers
+    
+    // timers
     std::vector<std::pair<ntl::nt::legacy_handle, async_operation*>> timers;
-		ntl::nt::user_thread timer_thread;
-		ntl::nt::srwlock timer_lock;
+    ntl::nt::user_thread timer_thread;
+    ntl::nt::srwlock timer_lock;
     ntl::nt::user_event timer_event;
     typedef ntl::nt::srwlock::guard<false> rlock;
     typedef ntl::nt::srwlock::guard<true>  wlock;
-	};
+  };
 
 
 }}}
