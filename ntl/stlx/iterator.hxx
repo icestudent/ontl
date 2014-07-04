f315179b40d8238fa9c25d48c1c91aa6f938904d
icestudent <icestudent@gmail.com>
2014-07-04 10:52:43 +0400

    stlx: N4017 container access implementation

--- ntl/stlx/iterator.hxx
+++ ntl/stlx/iterator.hxx
@@ -799,6 +799,36 @@ class ostreambuf_iterator
 
 ///\}
 
+  ///\name 24.8 Container access (N4017) [iterator.container]
+
+  template <class C> inline constexpr auto size(const C& c) noexcept { return c.size(); }
+
+  template <class T, size_t N> inline constexpr size_t size(const T (&array)[N]) noexcept { return N; }
+
+  template <class C> inline constexpr bool empty(const C& c) noexcept { return c.empty(); }
+
+  template <class T, size_t N> inline constexpr bool empty(const T (&array)[N]) noexcept { return false; }
+
+  template <class C> inline constexpr decltype(auto) front(C& c) { return c.front(); }
+
+  template <class C> inline constexpr decltype(auto) front(const C& c) { return c.front(); }
+
+  template <class C> inline constexpr decltype(auto) back(C& c) { return c.back(); }
+
+  template <class C> inline constexpr decltype(auto) back(const C& c) { return c.back(); }
+
+  template <class T, size_t N> inline constexpr T& front(T (&array)[N]) noexcept { return array[0]; }
+
+  template <class T, size_t N> inline constexpr T& back(T (&array)[N]) noexcept { return array[N-1]; }
+
+  template <class C> inline constexpr auto data(C& c) noexcept { return c.data(); }
+
+  template <class C> inline constexpr auto data(const C& c) noexcept { return c.data(); }
+
+  template <class T, size_t N> inline constexpr T* data(T (&array)[N]) noexcept { return array; }
+
+  ///\}
+
 /**@} lib_iterators */
 
 }//namespace std
