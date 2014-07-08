c4a8acf8b9ab8987095bd5eea9702563329764d3
icestudent <icestudent@gmail.com>
2014-07-08 13:05:34 +0400

    stlx: add priority_queue

--- ntl/stlx/priority_queue.hxx
+++ ntl/stlx/priority_queue.hxx
@@ -10,6 +10,7 @@
 
 #include "vector.hxx"
 #include "functional.hxx"
+#include "algorithm.hxx"
 
 namespace std {
 
@@ -35,26 +36,68 @@ namespace std {
     typedef typename Container::size_type       size_type;
   public:
   #ifdef NTL_CXX_RV
-    explicit priority_queue(const Compare& x = Compare(), Container&& = Container());
+    explicit priority_queue(const Compare& x, const Container& c);
+    explicit priority_queue(const Compare& x, Container&& = Container());
+    
     template <class InputIterator>
-    priority_queue(InputIterator first, InputIterator last, const Compare& x = Compare(), Container&& = Container());
-    priority_queue(priority_queue&&);
-    priority_queue& operator=(priority_queue&&);
+    priority_queue(InputIterator first, InputIterator last, const Compare& x, Container&& = Container());
+
+    template <class InputIterator>
+    priority_queue(InputIterator first, InputIterator last, const Compare& x, const Container& c);
+
+    priority_queue(priority_queue&& r)
+      : c(move(r.c))
+      , comp(move(r.comp))
+    {}
+
+    priority_queue& operator=(priority_queue&& r)
+    {
+      c = move(r.c);
+      comp = move(comp);
+      return *this;
+    }
+
   #else
-    explicit priority_queue(const Compare& x = Compare(), const Container& c = Container());
-    template <class InputIterator>
-    priority_queue(InputIterator first, InputIterator last, const Compare& x = Compare(), const Container& c = Container());
-    priority_queue(const priority_queue&);
-    priority_queue& operator= (const priority_queue&);
+    explicit priority_queue(const Compare& x, const Container& c = Container());
+
+
+    priority_queue(const priority_queue& r)
+      : c(r.c)
+      , comp(r.comp)
+    {}
+
+    priority_queue& operator= (const priority_queue& r)
+    {
+      c = r.c;
+      comp = r.comp;
+      return *this;
+    }
   #endif
+
+    explicit priority_queue()
+      : c()
+      , comp()
+    {}
+
+    template <class InputIterator>
+    priority_queue(InputIterator first, InputIterator last)
+      : c()
+      , comp()
+    {
+      c.insert(c.end(), first, last);
+      make_heap(c.begin(), c.end(), comp);
+    }
 
     template <class Alloc>
     explicit priority_queue(const Alloc&);
+
     template <class Alloc>
     priority_queue(const Compare&, const Alloc&);
+
     template <class Alloc>
     priority_queue(const Compare&, const Container&, const Alloc&);
     
+
   #ifdef NTL_CXX_RV
     template <class Alloc>
     priority_queue(const Compare&, Container&&, const Alloc&);
@@ -62,26 +105,64 @@ namespace std {
     priority_queue(priority_queue&&, const Alloc&);
   #endif
 
+
+    ///\name extensions
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    iterator begin()        { return c.begin(); }
+    iterator end()          { return c.end(); }
+
+    iterator begin()  const { return c.begin(); }
+    iterator end()    const { return c.end(); }
+
+    iterator cbegin() const { return c.begin(); }
+    iterator cend()   const { return c.end(); }
+    
+
+
+    ///\name comtainer access
     bool empty() const { return c.empty(); }
+
     size_type size() const { return c.size(); }
     
     const_reference top() const { return c.front(); }
-    void push(const value_type& x);
-    void pop();
+
+
+    ///\name modifying members
+    void push(const value_type& x)
+    {
+      c.push_back(x);
+      push_heap(c.begin(), c.end(), comp);
+    }
+
+    void pop()
+    {
+      pop_heap(c.begin(), c.end(), comp);
+      c.pop_back();
+    }
     
   #ifdef NTL_CXX_RV
-    void push(value_type&& x);
+    void push(value_type&& x)
+    {
+      c.push_back(std::move(x));
+      push_heap(c.begin(), c.end(), comp);
+    }
   #endif
   #ifdef NTL_CXX_VT
     template <class... Args>
-    void emplace(Args&&... args);
+    void emplace(Args&&... args)
+    {
+      c.emplace_back(std::forward<Args>(args)...);
+      push_heap(c.begin(), c.end(), comp);
+    }
   #endif
+    ///\}
 
-    
     void swap(priority_queue& x)
     {
-      c.swap(x.c);
       using std::swap;
+      swap(c, x.c);
       swap(comp, x.comp);
     }
 
