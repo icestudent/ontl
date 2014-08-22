b69b2368f4d2122fb857951d33dbf7fed8a46e43
icestudent <icestudent@gmail.com>
2014-08-22 14:22:39 +0400

    stlx: equal for 4 iterators

--- ntl/stlx/algorithm.hxx
+++ ntl/stlx/algorithm.hxx
@@ -211,8 +211,7 @@ bool
 }
 
 
-template
-<class InputIterator1, class InputIterator2, class BinaryPredicate>
+template<class InputIterator1, class InputIterator2, class BinaryPredicate>
 __forceinline
 bool
   equal(InputIterator1 first1, InputIterator1 last1,
@@ -223,6 +222,24 @@ bool
   return true;
 }
 
+template<class InputIterator1, class InputIterator2>
+__forceinline
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+{
+  for ( ; first1 != last1 && first2 != last2; ++first1, ++first2 )
+    if ( !(*first1 == *first2) ) return false;
+  return true;
+}
+
+
+template<class InputIterator1, class InputIterator2, class BinaryPredicate>
+__forceinline
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
+{
+  for ( ; first1 != last1 && first2 != last2; ++first1, ++first2 )
+    if ( !pred(*first1, *first2) ) return false;
+  return true;
+}
 
 template<class ForwardIterator1, class ForwardIterator2>
 __forceinline
