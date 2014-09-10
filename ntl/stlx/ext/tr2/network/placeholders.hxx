2ae2a75a012a27d62727eb90088ce444f5878288
icestudent <icestudent@gmail.com>
2014-09-10 16:53:49 +0400

    net: add placeholder for resolve iterator

--- ntl/stlx/ext/tr2/network/placeholders.hxx
+++ ntl/stlx/ext/tr2/network/placeholders.hxx
@@ -15,6 +15,7 @@ namespace std { namespace tr2 { namespace network {
   {
     static const std::placeholders::__::placeholder_t<1> error;
     static const std::placeholders::__::placeholder_t<2> bytes_transferred;
+    static const std::placeholders::__::placeholder_t<2> iterator;
   }
 }}}
 #endif // NTL__STLX_TR2_NETWORK_PLACEHOLDERS
