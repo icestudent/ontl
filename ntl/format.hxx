8ea84cce20efd3d74cb9fbd30b3009f38498d3d3
icestudent <icestudent@gmail.com>
2014-11-14 15:43:51 +0300

    update ntl/format.hxx and add to_hex for memory data conversation

--- ntl/format.hxx
+++ ntl/format.hxx
@@ -8,7 +8,7 @@
 #define NTL__FORMAT
 #pragma once
 
-#include "base.hxx"
+#include "basedef.hxx"
 
 namespace ntl {
 namespace fmt {
@@ -22,7 +22,7 @@ static inline
 char hex_digit(uint_t v)
 {
   static char h[] = "0123456789ABCDEF";
-  return h[v & 0xf];
+  return h[v & 0x0F];
 }
 
 
@@ -39,22 +39,33 @@ char_t * to_hex(int_t v, char_t * const p)
   return &p[2 + 2 * sizeof(int_t)];
 }
 
+template<typename char_t>
+static inline char_t* to_hex(char_t* dst, const void* src, size_t len)
+{
+  const uint8_t* p = reinterpret_cast<const uint8_t*>(src);
+  for(size_t i = 0; i < len; i++) {
+    const unsigned char c = *p++;
+    *dst++ = hex_digit(c);
+    *dst++ = hex_digit(c >> 4);
+  }
+  return dst - len*2;
+}
 
 template<typename char_t = char>
 struct hex_str_cast
 {
-    hex_str_cast(int8 v)    { to_hex(v, buf); }
-    hex_str_cast(int16 v)   { to_hex(v, buf); }
-    hex_str_cast(int32 v)   { to_hex(v, buf); }
-    hex_str_cast(int64 v)   { to_hex(v, buf); }
-    hex_str_cast(uint8 v)   { to_hex(v, buf); }
-    hex_str_cast(uint16 v)  { to_hex(v, buf); }
-    hex_str_cast(uint32 v)  { to_hex(v, buf); }
-    hex_str_cast(uint64 v)  { to_hex(v, buf); }
+    hex_str_cast(int8_t v)    { to_hex(v, buf); }
+    hex_str_cast(int16_t v)   { to_hex(v, buf); }
+    hex_str_cast(int32_t v)   { to_hex(v, buf); }
+    hex_str_cast(int64_t v)   { to_hex(v, buf); }
+    hex_str_cast(uint8_t v)   { to_hex(v, buf); }
+    hex_str_cast(uint16_t v)  { to_hex(v, buf); }
+    hex_str_cast(uint32_t v)  { to_hex(v, buf); }
+    hex_str_cast(uint64_t v)  { to_hex(v, buf); }
     operator const char_t * () { return buf; }
 
   private:
-    char_t    buf[sizeof(uint64) * 2 + sizeof("0x")];
+    char_t    buf[sizeof(uint64_t) * 2 + sizeof("0x")];
 };//struct hex_str_cast
 
 
@@ -86,14 +97,14 @@ char_t * to_dec(int_t v, char_t * const p)
 template<typename char_t = char>
 struct str_cast
 {
-    str_cast(int8 v)    { to_dec(v, buf); }
-    str_cast(int16 v)   { to_dec(v, buf); }
-    str_cast(int32 v)   { to_dec(v, buf); }
-    str_cast(int64 v)   { to_dec(v, buf); }
-    str_cast(uint8 v)   { to_dec(v, buf); }
-    str_cast(uint16 v)  { to_dec(v, buf); }
-    str_cast(uint32 v)  { to_dec(v, buf); }
-    str_cast(uint64 v)  { to_dec(v, buf); }
+    str_cast(int8_t v)    { to_dec(v, buf); }
+    str_cast(int16_t v)   { to_dec(v, buf); }
+    str_cast(int32_t v)   { to_dec(v, buf); }
+    str_cast(int64_t v)   { to_dec(v, buf); }
+    str_cast(uint8_t v)   { to_dec(v, buf); }
+    str_cast(uint16_t v)  { to_dec(v, buf); }
+    str_cast(uint32_t v)  { to_dec(v, buf); }
+    str_cast(uint64_t v)  { to_dec(v, buf); }
     operator const char_t * () { return buf; }
 
   private:
